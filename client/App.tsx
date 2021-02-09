828f2e33b9c40485709d7045463f072a39bdaa4d
webtree <web-tree@yandex.ru>
2021-02-09 11:22:44 +0400

    LP-6 добавил .eslintignore, .eslintrc, сделал необходимые настройки webpack.config.js, устранил замечания eslint

--- client/App.tsx
+++ client/App.tsx
@@ -1,36 +1,42 @@
-import React from "react";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import React from 'react';
+import {
+    BrowserRouter,
+    Route,
+    Switch,
+    Link,
+} from 'react-router-dom';
 
 import './App.css';
 
-export const App: React.FC = () => {
-    return (
-        <div>
-            <h1><span>Мой</span> апп.</h1>
+export const App: React.FC = () => (
+	<div>
+		<h1>
+			<span>Мой</span>
+			<b>апп.</b>
+		</h1>
 
-            <BrowserRouter>
-                <div>
-                    <Switch>
-                        <Route exact path="/">
-                            home
-                        </Route>
+		<BrowserRouter>
+			<div>
+				<Switch>
+					<Route exact path="/">
+						home
+					</Route>
 
-                        <Route path="/users">
-                            users
-                        </Route>
+					<Route path="/users">
+						users
+					</Route>
 
-                        <Route path="/about">
-                            about
-                        </Route>                                        
-                    </Switch>
-                </div>
+					<Route path="/about">
+						about
+					</Route>
+				</Switch>
+			</div>
 
-                <ul>
-                    <li><Link to="/">home</Link></li>
-                    <li><Link to="/users">users</Link></li>
-                    <li><Link to="/about">about</Link></li>
-                </ul>
-            </BrowserRouter>
-        </div>
-    );
-};
+			<ul>
+				<li><Link to="/">home</Link></li>
+				<li><Link to="/users">users</Link></li>
+				<li><Link to="/about">about</Link></li>
+			</ul>
+		</BrowserRouter>
+	</div>
+);
