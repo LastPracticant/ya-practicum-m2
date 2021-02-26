9ca563efd79948d2ba00f39bdf0bfa160b1efe50
Айрат <airat.must@yandex.ru>
2021-02-26 14:51:01 +0300

    добавил элемент snackbar

--- client/core/store/actions/auth.actions.ts
+++ client/core/store/actions/auth.actions.ts
@@ -4,40 +4,57 @@ import { ThunkAction } from 'redux-thunk';
 import { StoreProps } from '../store.types';
 import { hideLoaderAction, showLoaderAction } from './loader.actions';
 import { thunkCurrentUserInfo } from './profile.actions';
+import { visibleSnackBar } from './snackbar.actions';
 
-export const thunkLogin = (
-    data: SigninProps,
-): ThunkAction<void, StoreProps, unknown, Action<string>> => (
-    dispatch,
-) => {
-    dispatch(showLoaderAction());
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
 
-    AuthAPI.signin(data).finally(() => {
-        dispatch(hideLoaderAction());
-        dispatch(thunkCurrentUserInfo());
-    });
-};
+export const login = () => ({ type: LOGIN });
+
+export const logout = () => ({ type: LOGOUT });
 
 export const thunkSignup = (
     data: SignupProps,
-): ThunkAction<void, StoreProps, unknown, Action<string>> => (
-    dispatch,
-) => {
+): ThunkAction<void, StoreProps, unknown, Action<string>> => (dispatch) => {
     dispatch(showLoaderAction());
 
     AuthAPI.signup(data).finally(() => {
         dispatch(hideLoaderAction());
+        dispatch(login());
         dispatch(thunkCurrentUserInfo());
     });
 };
 
-export const thunkLogout = (
-): ThunkAction<void, StoreProps, unknown, Action<string>> => (
-    dispatch,
-) => {
+export const thunkLogout = (): ThunkAction<
+void,
+StoreProps,
+unknown,
+Action<string>
+> => (dispatch) => {
     dispatch(showLoaderAction());
 
     AuthAPI.logout().finally(() => {
+        dispatch(logout());
         dispatch(hideLoaderAction());
     });
+};
+
+export const thunkSignin = (
+    data: SigninProps,
+): ThunkAction<void, StoreProps, unknown, Action<string>> => (dispatch) => {
+    dispatch(showLoaderAction());
+    AuthAPI.signin(data).then(
+        () => {
+            dispatch(hideLoaderAction());
+            dispatch(login());
+            dispatch(thunkCurrentUserInfo());
+        },
+        (response) => {
+            dispatch(hideLoaderAction());
+            dispatch(visibleSnackBar({ type: 'error', msg: 'fdfsd' }));
+            response.json().then((result: any) => {
+                dispatch(visibleSnackBar({ type: 'error', msg: result?.reason }));
+            });
+        },
+    );
 };
