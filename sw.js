f44f5478d99f814bb2f124faddbeb8cd3ec28830
webtree <web-tree@yandex.ru>
2021-03-02 22:59:46 +0400

    LP-40 правки ревью

--- sw.js
+++ sw.js
@@ -18,53 +18,55 @@ const URLS = [
     'https://fonts.googleapis.com/icon?family=Material+Icons',
 ];
 
+const onlineFirstStrategy = (event) => caches.match(event.request)
+    .then((res) => {
+        const fetchRequest = event.request.clone();
+
+        return new Promise((resolve) => {
+            fetch(fetchRequest)
+                .then((response) => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return resolve(response);
+                    }
+
+                    const responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then((cache) => {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return resolve(response);
+                })
+                .catch((err) => {
+                    if (res) {
+                        return resolve(res);
+                    }
+                    console.error('Something is wrong', err);
+                });
+        });
+    });
+
+const fetchMiddleware = (event) => onlineFirstStrategy(event);
+
 this.addEventListener('install', (event) => {
     event.waitUntil(
         caches
             .open(CACHE_NAME)
             .then((cache) => {
-                console.log('Opened cache');
+                console.info('Opened cache');
                 return cache.addAll(URLS);
             })
             .catch((err) => {
-                console.log('Something is wrong', err);
+                console.error('Something is wrong', err);
             }),
     );
 });
 
 this.addEventListener('activate', (event) => {
-    console.log('activate', event);
+    console.info('activate', event);
 });
 
 this.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((res) => {
-                const fetchRequest = event.request.clone();
-
-                return new Promise((resolve) => {
-                    fetch(fetchRequest)
-                        .then((response) => {
-                            if (!response || response.status !== 200 || response.type !== 'basic') {
-                                return resolve(response);
-                            }
-
-                            const responseToCache = response.clone();
-
-                            caches.open(CACHE_NAME)
-                                .then((cache) => {
-                                    cache.put(event.request, responseToCache);
-                                });
-
-                            return resolve(response);
-                        })
-                        .catch((err) => {
-                            if (res) {
-                                return resolve(res);
-                            }
-                            console.log('Something is wrong', err);
-                        });
-                });
-            }),
-    );
+    event.respondWith(fetchMiddleware(event));
 });
