0235cdcd3d396e418410d343db39bc426d45284d
webtree <web-tree@yandex.ru>
2021-03-14 11:14:57 +0400

    LP-95 правки ревью

--- server/render.tsx
+++ server/render.tsx
@@ -1,7 +1,7 @@
 import { App } from 'client/App';
 import { composeStore, StoreProps } from 'client/core/store';
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToStaticMarkup, renderToString } from 'react-dom/server';
 import { Helmet, HelmetData } from 'react-helmet';
 import { Provider as ReduxProvider } from 'react-redux';
 import { StaticRouter } from 'react-router-dom';
@@ -14,29 +14,30 @@ interface PageHtmlProps {
 }
 
 function getPageHtml({ html, state, helmet }: PageHtmlProps) {
-    return `
-        <!DOCTYPE html>
+    const staticMarkup = renderToStaticMarkup(
         <html lang="ru">
-        <base href="/">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            ${helmet.title.toString()}
-            ${helmet.meta.toString()}
-            <link rel="icon" type="image/png" href="./idea.png"/>
-            <link rel="stylesheet" href="/main.css" type="text/css">
-        </head>
+            <base href="/" />
+            <head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+                ${helmet.title.toString()}
+                ${helmet.meta.toString()}
+                <link rel="icon" type="image/png" href="./idea.png" />
+                <link rel="stylesheet" href="/main.css" type="text/css" />
+            </head>
 
-        <body>
-            <div id="root">${html}</div>
-            <script>
-                window.__INITIAL_STATE__ = ${JSON.stringify(state)}
-            </script>
-            <script src="/app.js"></script>
-        </body>
-        </html>
-    `;
+            <body>
+                <div id="root">${html}</div>
+                <script>
+                    window.__INITIAL_STATE__ = ${JSON.stringify(state)}
+                </script>
+                <script src="/app.js" />
+            </body>
+        </html>,
+    );
+
+    return `<!DOCTYPE html> ${staticMarkup}`;
 }
 
 export const renderHtml = (reqUrl: string) => {
