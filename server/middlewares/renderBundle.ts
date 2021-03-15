35a751eb287544410e379f8d2674c8842e6391dc
webtree <web-tree@yandex.ru>
2021-03-15 19:48:29 +0400

    LP-86 временный

--- server/middlewares/renderBundle.ts
+++ server/middlewares/renderBundle.ts
@@ -1,10 +1,32 @@
+import {
+    composeStore,
+    // getCurrentUserInfoThunk,
+    // StoreProps,
+} from 'client/core/store';
 import { NextFunction, Request, Response } from 'express';
+// import { AnyAction } from 'redux';
+// import { ThunkDispatch } from 'redux-thunk';
+import { defaultState } from '../initialState';
 import { renderHtml } from './renderHtml';
 
-export function renderBundle(req: Request, res: Response, next: NextFunction) {
-    res.renderBundle = (url: string) => {
-        const { html } = renderHtml(url);
+import 'isomorphic-fetch';
 
+export function renderBundle(req: Request, res: Response, next: NextFunction) {
+    res.renderBundle = async (url: string) => {
+        const store = composeStore(defaultState);
+        const state = store.getState();
+
+        // const dispatch = store.dispatch as ThunkDispatch<StoreProps, void, AnyAction>;
+        // Promise.resolve().then(() => {
+        //     console.log('before dispatch getCurrentUserInfoThunk');
+        //     return new Promise(() => {
+        //         dispatch(getCurrentUserInfoThunk());
+        //     });
+        // }).finally(() => {
+        //     console.log('after dispatch getCurrentUserInfoThunk');
+        // });
+
+        const { html } = renderHtml(url, state, store);
         res.send(html);
     };
 
