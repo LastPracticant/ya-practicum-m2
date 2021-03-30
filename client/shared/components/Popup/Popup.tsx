6f3c97de185ed9b4d1467b389a0534a03e15d9f5
webtree <web-tree@yandex.ru>
2021-03-30 21:05:04 +0400

    LP-106 реализовал хук для управления видимостью элементов, реализовал базовый компонент модального окна

---
+++ client/shared/components/Popup/Popup.tsx
@@ -0,0 +1,44 @@
+import React, { FC, memo } from 'react';
+import './Popup.css';
+
+import { ComponentCommonProps, FnActionProps } from 'client/shared/types';
+import bem from 'bem-cn';
+import Modal from '@material-ui/core/Modal';
+import { Fade } from '@material-ui/core';
+import Backdrop from '@material-ui/core/Backdrop';
+import { Paper } from '../Paper';
+
+interface PopupProps extends ComponentCommonProps {
+    isVisible: boolean
+    onChangeVisible?: FnActionProps
+}
+
+const block = bem('Popup');
+
+export const Popup: FC<PopupProps> = memo(
+    ({
+        className,
+        isVisible,
+        onChangeVisible,
+        children,
+    }) => (
+        <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={block({}).mix(className).toString()}
+            open={isVisible}
+            onClose={onChangeVisible}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+                timeout: 500,
+            }}
+        >
+            <Fade in={isVisible}>
+                <Paper sizes="small">
+                    {children}
+                </Paper>
+            </Fade>
+        </Modal>
+    ),
+);
