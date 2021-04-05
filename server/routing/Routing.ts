7df8128c0a0bc0c4c3e52bff5a9c28d8470fd8c7
webtree <web-tree@yandex.ru>
2021-04-05 23:08:15 +0400

    LP-109 добавил в роутинг контроллеры

--- server/routing/Routing.ts
+++ server/routing/Routing.ts
@@ -1,121 +1,22 @@
 import express, { Express } from 'express';
 import path from 'path';
-import { ExpressOAuthAPI } from 'server/api/oauth.api';
-import { ExpressProfileAPI } from 'server/api/profile.api';
-
-import { ExpressAuthAPI } from '../api/auth.api';
-import { getHeadersWithCookies, setCookies } from '../server.utils';
+import { AuthController, ProfileController } from '../controllers';
 
 export function routing(app: Express) {
     const jsonParser = express.json();
 
     app.use(express.static(path.join(__dirname, './dist')));
 
-    app.get('/api/v2/auth/user', (req, res) => {
-        ExpressAuthAPI.getCurrentUserInfo({
-            headers: getHeadersWithCookies(req),
-        })
-            .then(async (response) => {
-                res.send(await response.json());
-            })
-            .catch((error) => {
-                res.status(error.status).send(error.statusText);
-            });
-    });
+    app.get('/api/v2/auth/user', AuthController.checkAuth);
+    app.get('/api/v2/oauth/yandex/service-id', AuthController.OAuthGetServiceId);
+    app.post('/api/v2/oauth/yandex', jsonParser, AuthController.OAuth);
+    // TODO: сломалась авторизация
+    app.post('/api/v2/auth/signin', jsonParser, AuthController.Signin);
+    app.post('/api/v2/auth/signup', jsonParser, AuthController.Signup);
+    app.post('/api/v2/auth/logout', jsonParser, AuthController.logout);
 
-    app.get('/api/v2/oauth/yandex/service-id', (req, res) => {
-        ExpressOAuthAPI.getServiceId()
-            .then(async (response) => {
-                res.send(await response.json());
-            })
-            .catch((error) => {
-                res.status(error.status).send(error.statusText);
-            });
-    });
-
-    app.post('/api/v2/oauth/yandex', jsonParser, (req, res) => {
-        if (!req.body) return res.sendStatus(400);
-
-        ExpressOAuthAPI.signinWithYandex(req.body)
-            .then(async (fetchResponse) => {
-                setCookies(fetchResponse, res);
-
-                res.send(await fetchResponse.text());
-            })
-            .catch((error) => {
-                res.status(error.status).send(error.statusText);
-            });
-    });
-
-    app.post('/api/v2/auth/signin', jsonParser, (req, res) => {
-        if (!req.body) return res.sendStatus(400);
-
-        ExpressAuthAPI.signin(req.body)
-            .then(async (fetchResponse) => {
-                setCookies(fetchResponse, res);
-
-                res.send(await fetchResponse.text());
-            })
-            .catch((error) => {
-                res.status(error.status).send(error.statusText);
-            });
-    });
-
-    app.post('/api/v2/auth/signup', jsonParser, (req, res) => {
-        if (!req.body) return res.sendStatus(400);
-
-        ExpressAuthAPI.signup(req.body)
-            .then(async (fetchResponse) => {
-                setCookies(fetchResponse, res);
-
-                res.send(await fetchResponse.text());
-            })
-            .catch((error) => {
-                res.status(error.status).send(error.statusText);
-            });
-    });
-
-    app.post('/api/v2/auth/logout', jsonParser, (req, res) => {
-        ExpressAuthAPI.logout({
-            headers: getHeadersWithCookies(req),
-        })
-            .then(async (response) => {
-                res.clearCookie('uuid');
-                res.clearCookie('authCookie');
-                res.send(await response.text());
-            })
-            .catch((error) => {
-                res.status(error.status).send(error.statusText);
-            });
-    });
-
-    app.put('/api/v2/user/profile', jsonParser, (req, res) => {
-        if (!req.body) return res.sendStatus(400);
-
-        ExpressProfileAPI.change(req.body, {
-            headers: getHeadersWithCookies(req),
-        })
-            .then(async (response) => {
-                res.send(await response.json());
-            })
-            .catch((error) => {
-                res.status(error.status).send(error.statusText);
-            });
-    });
-
-    app.put('/api/v2/user/password', jsonParser, (req, res) => {
-        if (!req.body) return res.sendStatus(400);
-
-        ExpressProfileAPI.changePassword(req.body, {
-            headers: getHeadersWithCookies(req),
-        })
-            .then(async (response) => {
-                res.send(await response.json());
-            })
-            .catch((error) => {
-                res.status(error.status).send(error.statusText);
-            });
-    });
+    app.put('/api/v2/user/profile', jsonParser, ProfileController.change);
+    app.put('/api/v2/user/password', jsonParser, ProfileController.changePassword);
 
     app.get('*', (req, res) => {
         res.renderBundle(req.url);
