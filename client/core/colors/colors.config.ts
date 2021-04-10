31966225baa6909a34f6d616a05054168f1e91b2
webtree <web-tree@yandex.ru>
2021-04-10 18:00:51 +0400

    LP-103 дополнил конфиг

--- client/core/colors/colors.config.ts
+++ client/core/colors/colors.config.ts
@@ -1,14 +1,15 @@
 import { PaletteOptions } from '@material-ui/core/styles/createPalette';
+import { ColorThemes } from '../context/ColorTheme';
 
 export const DARK_THEME: PaletteOptions = {
-    type: 'dark',
+    type: ColorThemes.Dark,
     primary: {
         main: '#fff',
     },
 };
 
 export const LIGHT_THEME: PaletteOptions = {
-    type: 'light',
+    type: ColorThemes.Light,
     primary: {
         main: '#000',
     },
