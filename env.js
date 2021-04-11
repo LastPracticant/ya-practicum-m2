e001a8899d6f9f226602d2c35c505b3d288b5ba0
webtree <web-tree@yandex.ru>
2021-04-11 14:16:35 +0400

    LP-115 поправил url редиректа oauth heroku, поправил стили кнопок

--- env.js
+++ env.js
@@ -12,7 +12,7 @@ const postgresConnectOptions = {
 module.exports = {
     IS_DEV: isDev,
     MONGO_HOST: `mongodb://${isDev ? 'localhost' : 'mongo'}:27017`,
-    APP_PROD_URL: 'https://last-practicant.herokuapp.com',
+    APP_PROD_URL: 'https://last-practicant.herokuapp.com/',
     APP_DEV_URL: 'http://localhost:5000',
     POSTGRES_CONNECT_URL: isDev ? process.env.POSTGRES_DEVELOPMENT : process.env.POSTGRES_PRODUCTION,
     POSTGRES_CONNECT_OPTIONS: !isDev ? postgresConnectOptions : {},
