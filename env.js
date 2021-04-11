738a7e44d42fdc6446980052fb174e22fd72554a
webtree <web-tree@yandex.ru>
2021-04-11 10:48:46 +0400

    LP-115 изолировал mongo до лучших времен

--- env.js
+++ env.js
@@ -5,12 +5,5 @@ module.exports = {
     MONGO_HOST: `mongodb://${isDev ? 'localhost' : 'mongo'}:27017`,
     APP_PROD_URL: 'https://last-practicant.herokuapp.com',
     APP_DEV_URL: 'http://localhost:5000',
-    POSTGRES_CONNECT_OPTIONS: {
-        host: 'localhost',
-        port: 5436,
-        username: 'postgres',
-        password: process.env.POSTGRES_PASSWORD,
-        database: 'postgres',
-        dialect: 'postgres',
-    },
+    POSTGRES_CONNECT_OPTIONS: isDev ? process.env.POSTGRES_DEVELOPMENT : process.env.POSTGRES_PRODUCTION,
 };
--- server/server.ts
+++ server/server.ts
@@ -3,16 +3,13 @@ import devMiddleware from 'webpack-dev-middleware';
 import hotMiddleware from 'webpack-hot-middleware';
 import cookieParser from 'cookie-parser';
 import webpack, { Configuration } from 'webpack';
-import { MongoClient } from 'mongodb';
 
-import { MONGO_HOST } from '../env';
 import webpackConfig from '../webpack.config.client';
 import { renderBundle } from './middlewares/renderBundle';
 import { routing } from './routing';
 import { postgres } from './models';
 
 const compiler = webpack(webpackConfig as Configuration);
-const mongo = new MongoClient(MONGO_HOST);
 
 export class Server {
     private app;
@@ -37,16 +34,6 @@ export class Server {
 
     private dbConnect() {
         postgres.sync();
-
-        mongo.connect((err) => {
-            if (err) {
-                console.error('--------------- MongoDB connection error. ---------------');
-
-                throw err;
-            }
-
-            console.info('--------------- MongoDB connected. ---------------');
-        });
     }
 
     private routerConfig() {
