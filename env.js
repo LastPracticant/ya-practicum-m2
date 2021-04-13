586203f3fabf0ce1fc962adc470c48af20284305
webtree <web-tree@yandex.ru>
2021-04-13 09:46:29 +0400

    LP-115 немного поправил конфиги, правки ревь

--- env.js
+++ env.js
@@ -1,6 +1,7 @@
 const isDev = process.env.NODE_ENV !== 'production';
 
-const postgresConnectOptions = {
+const postgresProdConnectOptions = {
+    protocol: 'postgres',
     dialectOptions: {
         ssl: {
             require: true,
@@ -14,6 +15,7 @@ module.exports = {
     MONGO_HOST: `mongodb://${isDev ? 'localhost' : 'mongo'}:27017`,
     APP_PROD_URL: 'https://last-practicant.herokuapp.com/',
     APP_DEV_URL: 'http://localhost:5000',
-    POSTGRES_CONNECT_URL: isDev ? process.env.POSTGRES_DEVELOPMENT : process.env.POSTGRES_PRODUCTION,
-    POSTGRES_CONNECT_OPTIONS: !isDev ? postgresConnectOptions : {},
+    POSTGRES_CONNECT_OPTIONS: !isDev ? postgresProdConnectOptions : {
+        port: 5436,
+    },
 };
--- server/models/index.ts
+++ server/models/index.ts
@@ -2,7 +2,7 @@ import {
     Sequelize,
 } from 'sequelize-typescript';
 
-import { POSTGRES_CONNECT_URL, POSTGRES_CONNECT_OPTIONS } from '../../env';
+import { POSTGRES_CONNECT_OPTIONS } from '../../env';
 import { TopicModel } from './TopicModel';
 import { CommentModel } from './CommentModel';
 import { UserModel } from './UserModel';
@@ -17,11 +17,16 @@ class PostgresConnector {
     users: UserModel;
 
     constructor() {
-        this.sequelize = new Sequelize(POSTGRES_CONNECT_URL as string, {
-            dialect: 'postgres',
-            protocol: 'postgres',
-            ...POSTGRES_CONNECT_OPTIONS,
-        });
+        this.sequelize = new Sequelize(
+            process.env.POSTGRES_DATABASE as string,
+            process.env.POSTGRES_USERNAME as string,
+            process.env.POSTGRES_PASSWORD as string,
+            {
+                host: 'localhost',
+                dialect: 'postgres',
+                ...POSTGRES_CONNECT_OPTIONS,
+            },
+        );
         this.comments = new CommentModel(this.sequelize);
         this.topics = new TopicModel(this.sequelize);
         this.users = new UserModel(this.sequelize);
